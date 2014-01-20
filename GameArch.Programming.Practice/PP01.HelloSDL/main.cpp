8bff89b9b6521ff82df753308cb92183840fc522
GiHeonHan <kihanlll@gmail.com>
2014-01-20 16:48:54 +0900

    ~ project 3

--- GameArch.Programming.Practice/PP01.HelloSDL/main.cpp
+++ GameArch.Programming.Practice/PP01.HelloSDL/main.cpp
@@ -1,31 +1,18 @@
-#include<SDL.h>
+#include "Game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+Game* g_game = 0; // our Game object
 
-int main(int argc, char* args[])
+int main(int argc, char* argv[])
 {
-    // SDL 초기화
-    if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-    {
-	// 초기화 성공하면, window 생성
-	g_pWindow = SDL_CreateWindow("PP01.HelloSDL",
-		SDL_WINDOWPOS_CENTERED, 
-		SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_SHOWN);
-  	// window 생성을 성공하면, renderer 생성 
-	if(g_pWindow != 0) {	
-	      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-	}
-    } else {
-	return 1; // sdl could not initialize
-    }
+    g_game = new Game();
+    g_game->init("Chapter 1", 100, 100, 640, 480, false);
 
-     // 색 (R,G,B,A) 지정 
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);           
-    SDL_RenderClear(g_pRenderer);      // 윈도우를 지정된 색으로 지움 
-    SDL_RenderPresent(g_pRenderer);   // 윈도우 보이기
-    SDL_Delay(5000);                          // 5초 동안 지연
-    SDL_Quit();                                   //  SDL 정리 
-    return 0;
+    while(g_game->running())
+    {
+	g_game->handleEvents();
+	g_game->update();
+	g_game->render();
+    }
+    g_game->clean();
+    return 0; 
 }
